da0c2af711f23195b0aacf939744d5c4fc772eee
Hamza Assada <7amza.it@gmail.com>
2023-05-01 00:01:09 +0300

    Creat UserMan

---
+++ lib/main/data/server/user_manager.dart
@@ -0,0 +1,34 @@
+import 'package:d2_remote/modules/auth/user/entities/user.entity.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../usescases/bundle/bundle.dart';
+
+abstract class UserManager {
+  Future<User?> logIn(String username, String password, String serverUrl,
+      {Future<SharedPreferences>? sharedPreferenceInstance,
+      bool? inMemory,
+      Dio? dioTestClient});
+
+  Future<User?> handleAuthData(
+      {String serverUrl, Bundle? data, int? requestCode});
+
+  Future<bool> isUserLoggedIn(
+      {Future<SharedPreferences>? sharedPreferenceInstance, bool? inMemory});
+
+  Future<String> userInitials();
+
+  Future<String> userFullName();
+
+  Future<String> userName();
+
+  // D2 getD2();
+
+  Future<bool> hasMultipleAccounts();
+
+  // Future<Pair<String, Integer>> getTheme();
+
+  Future<bool> logout();
+
+  Future<bool> allowScreenShare();
+}
---
+++ lib/main/data/server/user_manager_impl.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_manager_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userManagerHash() => r'cb684d22a8b5ab968f83692f9db973341768e505';
+
+/// See also [userManager].
+@ProviderFor(userManager)
+final userManagerProvider = AutoDisposeProvider<UserManager>.internal(
+  userManager,
+  name: r'userManagerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserManagerRef = AutoDisposeProviderRef<UserManager>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
