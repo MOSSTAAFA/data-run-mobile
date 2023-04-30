2452963dd995fc63d6c7a3d1cff2e1baea2dfa8a
Hamza Assada <7amza.it@gmail.com>
2023-05-01 00:00:39 +0300

    Creat Main Nav

---
+++ lib/main/usescases/main/main_navigator.dart
@@ -0,0 +1,14 @@
+enum MainScreenNavItem {
+  PROGRAMS, //('R.done_task', R.id.menu_home),
+  VISUALIZATIONS, //(R.string.done_task, R.id.menu_home),
+  QR, //(R.QR_SCANNER, R.id.qr_scan),
+  SETTINGS, //(R.SYNC_MANAGER, R.id.sync_manager),
+  TROUBLESHOOTING, //(R.main_menu_troubleshooting, R.id.menu_troubleshooting),
+  JIRA, //(R.jira_report, R.id.menu_jira),
+  ABOUT, //(R.about, R.id.menu_about);
+
+  // const MainScreen(this.title, this.navViewId);
+
+  // final String title;
+  // final ViewAction navViewId;
+}
