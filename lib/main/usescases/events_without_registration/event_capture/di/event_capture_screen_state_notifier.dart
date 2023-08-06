5d19dfaf4f78c46efaeb90a7923467b8ac7acdba
Hamza <hamza@nmcpye.org>
2023-08-06 22:43:45 +0300

    add event cap screen

---
+++ lib/main/usescases/events_without_registration/event_capture/di/event_capture_screen_state_notifier.dart
@@ -0,0 +1,79 @@
+import 'package:equatable/equatable.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'event_capture_screen_state_notifier.g.dart';
+
+@riverpod
+class EventCaptureScreenStateNotifier
+    extends _$EventCaptureScreenStateNotifier {
+  @override
+  EventCaptureScreenState build() {
+    // this.key = key;
+    return const EventCaptureScreenState();
+  }
+
+  void showProgress() {
+    state = state.copyWith(progress: true);
+  }
+
+  void hideProgress() {
+    state = state.copyWith(progress: false);
+  }
+
+  void hideNavigationBar() {
+    state = state.copyWith(visibleNavigationBar: false);
+  }
+
+  void updatePercentage(double percentage) {
+    state = state.copyWith(percentage: percentage);
+  }
+
+// /// set index(int x)
+// void selectTabIndex(int index) {
+//   state = state.copyWith(index: index);
+// }
+//
+// void selectTabView(ViewAction viewAction) {}
+//
+// void hideBottomNavBar([bool? hide]) {
+//   state = state.copyWith(hideBottomNavBar: hide ?? !state.hideBottomNavBar);
+// }
+//
+// void hideActionBtn([bool? hide]) {
+//   state = state.copyWith(hideActionBtn: hide ?? !state.hideActionBtn);
+// }
+}
+
+class EventCaptureScreenState with EquatableMixin {
+  const EventCaptureScreenState(
+      [this.progress = true,
+      this.visibleNavigationBar = false,
+      this.percentage = 0.0,
+      this.hasExpired = false]);
+
+  final bool progress;
+  final bool visibleNavigationBar;
+  final double percentage;
+  final bool hasExpired;
+
+  @override
+  List<Object?> get props =>
+      [progress, visibleNavigationBar, percentage, hasExpired];
+
+  EventCaptureScreenState copyWith({
+    bool? progress,
+    bool? visibleNavigationBar,
+    double? percentage,
+    bool? hasExpired,
+  }) {
+    return EventCaptureScreenState(
+      progress ?? this.progress,
+      visibleNavigationBar ?? this.visibleNavigationBar,
+      percentage ?? this.percentage,
+      hasExpired ?? this.hasExpired,
+    );
+  }
+
+  @override
+  bool? get stringify => true;
+}
---
+++ lib/main/usescases/events_without_registration/event_capture/di/event_capture_screen_state_notifier.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_capture_screen_state_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$eventCaptureScreenStateNotifierHash() =>
+    r'10f85de915cea41950d1731b3acb46cb9026b031';
+
+/// See also [EventCaptureScreenStateNotifier].
+@ProviderFor(EventCaptureScreenStateNotifier)
+final eventCaptureScreenStateNotifierProvider = AutoDisposeNotifierProvider<
+    EventCaptureScreenStateNotifier, EventCaptureScreenState>.internal(
+  EventCaptureScreenStateNotifier.new,
+  name: r'eventCaptureScreenStateNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$eventCaptureScreenStateNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EventCaptureScreenStateNotifier
+    = AutoDisposeNotifier<EventCaptureScreenState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
