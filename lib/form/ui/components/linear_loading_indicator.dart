f10c63b52d1aa82418952975301f0a2aeac0baf1
Hamza <hamza@nmcpye.org>
2023-08-06 22:42:43 +0300

    temporarily change width

--- lib/form/ui/components/linear_loading_indicator.dart
+++ lib/form/ui/components/linear_loading_indicator.dart
@@ -4,18 +4,26 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../utils/mass_utils/colors.dart';
 
 class LinearLoadingIndicator extends ConsumerWidget {
-  const LinearLoadingIndicator({super.key, this.isLoading = false});
+  const LinearLoadingIndicator({
+    super.key,
+    this.isLoading = false,
+    this.backgroundColor,
+  });
+
   final bool isLoading;
+  final Color? backgroundColor;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox(
-      height: 6,
+    return Container(
+      color: backgroundColor,
+      height: 10,
       child: isLoading
           ? LinearProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(
                   convertHexStringToColor('#FF9800')!),
               semanticsLabel: 'Loading progress indicator',
-              minHeight: 2,
+              minHeight: 8,
             )
           : null,
     );
