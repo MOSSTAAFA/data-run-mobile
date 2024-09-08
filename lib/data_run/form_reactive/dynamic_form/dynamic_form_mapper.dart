2fcc645513f37867d418082fb0305057537ceabc
Hamza-ye <7amza.it@gmail.com>
2024-09-09 00:37:25 +0300

    Implement formElements travers reactive

--- lib/data_run/form_reactive/dynamic_form/dynamic_form_mapper.dart
+++ lib/data_run/form_reactive/dynamic_form/dynamic_form_mapper.dart
@@ -354,34 +354,3 @@ class ElementProperties with EquatableMixin {
       [label, placeholder, disabled, readonly, mandatory, order];
 }
 
-class FormElementNotFoundException extends DException {
-  /// The name of the element that was not found.
-  final String? elementName;
-
-  FormElementNotFoundException({this.elementName});
-
-  @override
-  String toString() {
-    if (elementName == null) {
-      return 'FormElementNotFoundException: element not found.';
-    }
-
-    return 'FormElementNotFoundException: element with name: \'$elementName\' not found.';
-  }
-}
-
-class FormRepeatElementInvalidIndexException extends DException {
-  /// The invalid index that was the cause of this exception.
-  final String index;
-
-  /// Creates an instance of the exception.
-  ///
-  /// The [index] argument is the invalid index that was the cause
-  /// of this exception.
-  FormRepeatElementInvalidIndexException(this.index);
-
-  @override
-  String toString() {
-    return 'FormRepeatElementInvalidIndexException: Index \'$index\' is not a valid index for FormRepeatElement';
-  }
-}
