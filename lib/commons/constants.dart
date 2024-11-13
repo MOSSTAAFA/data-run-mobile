54ef8511de61cac8e28c741a2d6672b24802fd42
Hamza-ye <7amza.it@gmail.com>
2024-11-13 17:41:30 +0300

    use https

--- lib/commons/constants.dart
+++ lib/commons/constants.dart
@@ -7,7 +7,7 @@ class Constants {
   static const String INCIDENT_DATE = 'incidentDate';
 }
 
-const String kApiBaseUrl = 'http://api.nmcpye.org';
+const String kApiBaseUrl = 'https://api.nmcpye.org';
 // const String kApiBaseUrl = 'http://10.0.2.2:8080'; // Vm network
 // const String kApiBaseUrl = 'http://192.168.8.110:8080'; // hNet
 // const String kApiBaseUrl = 'http://192.168.137.1:8080'; // mobile hotspot
